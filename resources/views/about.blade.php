af9875cf526a7ff03fd5864c7fd3ec2d89aac28d
rkozlov95 <rkozlov95@mail.ru>
2019-11-25 00:48:58 +0300

    Create layout and create file app.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,10 @@
-<h1>About blog</h1>
-<p>Experiments with laravel</p>
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>About blog</h1>
+	<p>Experiments with laravel</p>
+@endsection
