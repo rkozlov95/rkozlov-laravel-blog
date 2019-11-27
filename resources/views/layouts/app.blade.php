fb9a4fe37e7fb5de3e6e754db62009d444843dac
rkozlov95 <rkozlov95@mail.ru>
2019-11-27 23:16:45 +0300

    fix link on app.css for heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
         <title>Rkozlov Blog - @yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="csrf-param" content="_token" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
+        <link href="css/app.css" rel="stylesheet">
+        <script src="js/app.js"></script>
     </head>
     <body>
         <header>
