9386117b706b25ddd1e559672c014142db136002
rkozlov95 <rkozlov95@mail.ru>
2019-12-01 15:20:42 +0300

    Include library laravelCollective

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$article->name}}</h1>
-    <div>{{$article->body}}</div>
+    <div class="shadow-sm p-3 mb-5 bg-white rounded">
+        <h1>{{$article->name}}</h1>
+        <div>{{$article->body}}</div>
+    </div>
 @endsection
+
